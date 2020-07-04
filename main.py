29f1458be23e49c90176daf6bf7429d7d89ead66
DariDie <daridie@mail.ru>
2020-07-04 18:02:20 +0500

    done

--- main.py
+++ main.py
@@ -60,15 +60,15 @@ def text(message):
 		try:
 			res = requests.get("http://api.openweathermap.org/data/2.5/weather", params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 			data = res.json()
-			print("conditions:", data['weather'][0]['description'])
-			print("temp:", data['main']['temp'])
-			print("temp_min:", data['main']['temp_min'])
-			print("temp_max:", data['main']['temp_max'])
+			#print("conditions:", data['weather'][0]['description'])
+			#print("temp:", data['main']['temp'])
+			#print("temp_min:", data['main']['temp_min'])
+			#print("temp_max:", data['main']['temp_max'])
 
-			bot.send_message(message.from_user.id, "Условия:" + data['weather'][0]['description'])
-			#bot.send_message(message.from_user.id, 'Ссылка на маршрут: https://i.ibb.co/GsVDNQH/0-Lus2-Gx-Qesk.jpg')
-			#bot.send_message(message.from_user.id, 'Ссылка на маршрут: https://i.ibb.co/GsVDNQH/0-Lus2-Gx-Qesk.jpg')
-			#bot.send_message(message.from_user.id, 'Ссылка на маршрут: https://i.ibb.co/GsVDNQH/0-Lus2-Gx-Qesk.jpg')
+			bot.send_message(message.from_user.id, "Условия: " + data['weather'][0]['description'])
+			bot.send_message(message.from_user.id, "Температура: " + data['main']['temp'])
+			bot.send_message(message.from_user.id, "Максимальная температура:" + data['main']['temp_min'])
+			bot.send_message(message.from_user.id, "Минимальная температура:" + data['main']['temp_max'])
 		except Exception as e:
 			print("Exception (weather):", e)
 			pass

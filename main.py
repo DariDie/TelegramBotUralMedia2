7256ba1255c0a5dd36b7423c266d771f6872322a
DariDie <daridie@mail.ru>
2020-07-04 18:05:03 +0500

    done

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ def text(message):
 		try:
 			res = requests.get("http://api.openweathermap.org/data/2.5/weather", params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 			data = res.json()
-			#print("conditions:", data['weather'][0]['description'])
-			#print("temp:", data['main']['temp'])
-			#print("temp_min:", data['main']['temp_min'])
-			#print("temp_max:", data['main']['temp_max'])
+			print("conditions:", data['weather'][0]['description'])
+			print("temp:", data['main']['temp'])
+			print("temp_min:", data['main']['temp_min'])
+			print("temp_max:", data['main']['temp_max'])
 
 			bot.send_message(message.from_user.id, "Условия: " + data['weather'][0]['description'])
 			bot.send_message(message.from_user.id, "Температура: " + data['main']['temp'])
